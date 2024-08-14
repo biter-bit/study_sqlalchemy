f74f6732642c96d8e78357efaf11773f08d9ae2c
Миша Курашев <kurashevmichael@gmail.com>
2024-08-14 19:19:26 +0300

    create

---
+++ database.py
@@ -0,0 +1,4 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy import URL, create_engine, text
+from config import settings
