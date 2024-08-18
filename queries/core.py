62b1ed7a30d03373c7a1a5e48c8cdf334366e2fc
Миша Курашев <kurashevmichael@gmail.com>
2024-08-18 17:58:27 +0300

    add new query orm and core

--- queries/core.py
+++ queries/core.py
@@ -1,4 +1,4 @@
-from sqlalchemy import text, insert
+from sqlalchemy import text, insert, select, update
 from database import sync_engine, async_engine
 from models import metadata_obj, workers_table
 import asyncio
@@ -8,6 +8,8 @@ from models import Base
 
 # text - функция преобразовывает текст запроса в вид, который понимает sqlalchemy при использовании сырых запросов
 # insert - функция преобразовывает готовую таблицу для дальнейшей вставки информации в нее
+# Base - базовый класс модели
+# select - функция преобразовывает готовую таблицу для дальнейшего получения данных с нее
 
 # ----------------------------------------------------------------------------
 
@@ -20,81 +22,120 @@ def create_tables():
     Base.metadata.create_all() # создать все таблицы
     sync_engine.echo = True # включить логи
 
-# создание синхронной сессии с rollback для выполнения запросов к б.д.
-def get_123_sync():
-    with sync_engine.connect() as conn:
-        # выполнение запроса к б.д.
-        res = conn.execute(
-            text("SELECT VERSION()") # сырой запрос можно писать только в text
-        )
+class QueryCoreV1:
+    """Класс представляет интерфейс по взаимодействию с бд с помощью сырых запросов"""
 
-        # работает с данными в оперативной памяти, выводит все строки
-        print(f"{res.all()=}")
-
-        # работает с данными в оперативной памяти, выводит первую строку
-        print(f"{res.first()=}")
-
-        # выполнить сохранение в бд
-        conn.commit()
-
-    # создание синхронной сессии с commit для выполнения запросов к б.д.
-    # with sync_engine.begin() as conn:
-    #     # выполнение запроса к б.д.
-    #     res = conn.execute(
-    #         text("SELECT VERSION()")  # сырой запрос можно писать только в text
-    #     )
-    #
-    #     # работает с данными в оперативной памяти, выводит все строки
-    #     print(f"{res.all()=}")
-    #
-    #     # работает с данными в оперативной памяти, выводит первую строку
-    #     print(f"{res.first()=}")
-
-# создание асинхронной сессии с rollback для выполнения запросов к б.д.
-async def get_123_async():
-    async with async_engine.connect() as conn:
-        # выполнение запроса к б.д.
-        res = conn.execute(
-            text("SELECT VERSION()") # сырой запрос можно писать только в text
-        )
-
-        # работает с данными в оперативной памяти, выводит все строки
-        print(f"{res.all()=}")
-
-        # работает с данными в оперативной памяти, выводит первую строку
-        print(f"{res.first()=}")
-
-        # выполнить сохранение в бд
-        conn.commit()
-
-# создание синхронной сессии с rollback для выполнения запросов к б.д. (в стиле сырого запроса)
-def insert_data():
-    with sync_engine.connect() as conn:
-        # выполнение запроса к б.д.
-        conn.execute(
-            text("INSERT INTO workers (username) VALUES ('Bobr'), ('Volk');") # сырой запрос можно писать только в text
-        )
-
-        # выполнить сохранение в бд
-        conn.commit()
-
-# создание синхронной сессии с rollback для выполнения запросов к б.д. (в стиле query builder)
-def insert_data_query_builder():
-    with sync_engine.connect() as conn:
-        # выполнение запроса к б.д. в стиле query builder
-        conn.execute(
-            insert(workers_table).values(
-                [
-                    {"username": "Bobr"},
-                    {"username": "Volk"}
-                ]
+    # создание синхронной сессии с rollback для выполнения запросов к б.д.
+    @staticmethod
+    def get_123_sync():
+        with sync_engine.connect() as conn:
+            # выполнение запроса к б.д.
+            res = conn.execute(
+                text("SELECT VERSION()") # сырой запрос можно писать только в text
             )
-        )
 
-        # выполнить сохранение в бд
-        conn.commit()
+            # работает с данными в оперативной памяти, выводит все строки
+            print(f"{res.all()=}")
 
-# ----------------------------------------------------------------------
+            # работает с данными в оперативной памяти, выводит первую строку
+            print(f"{res.first()=}")
 
-# запуск асинхронной функции
-asyncio.run(get_123_async())
+            # выполнить сохранение в бд
+            conn.commit()
+
+        # создание синхронной сессии с commit для выполнения запросов к б.д.
+        # with sync_engine.begin() as conn:
+        #     # выполнение запроса к б.д.
+        #     res = conn.execute(
+        #         text("SELECT VERSION()")  # сырой запрос можно писать только в text
+        #     )
+        #
+        #     # работает с данными в оперативной памяти, выводит все строки
+        #     print(f"{res.all()=}")
+        #
+        #     # работает с данными в оперативной памяти, выводит первую строку
+        #     print(f"{res.first()=}")
+
+    # создание асинхронной сессии с rollback для выполнения запросов к б.д.
+    @staticmethod
+    async def get_123_async():
+        async with async_engine.connect() as conn:
+            # выполнение запроса к б.д.
+            res = conn.execute(
+                text("SELECT VERSION()") # сырой запрос можно писать только в text
+            )
+
+            # работает с данными в оперативной памяти, выводит все строки
+            print(f"{res.all()=}")
+
+            # работает с данными в оперативной памяти, выводит первую строку
+            print(f"{res.first()=}")
+
+            # выполнить сохранение в бд
+            conn.commit()
+
+    @staticmethod
+    def insert_data():
+        """Вставка значения в бд"""
+        with sync_engine.connect() as conn:
+            # выполнение запроса к б.д.
+            conn.execute(
+                text("INSERT INTO workers (username) VALUES ('Bobr'), ('Volk');") # сырой запрос можно писать только в text
+            )
+
+            # выполнить сохранение в бд
+            conn.commit()
+
+    @staticmethod
+    def update_data(worker_id: int = 2, name: str = "Misha"):
+        """Обновление значения в бд"""
+        with sync_engine.connect() as conn:
+            stmt = text('UPDATE workers SET username=:username WHERE id=:id')
+            stmt = stmt.bindparams(username=name, id=worker_id)
+            conn.execute(stmt)
+            conn.commit()
+
+class QueryCoreV2:
+    """Класс представляет интерфейс по взаимодействию с бд в стиле query builder"""
+    @staticmethod
+    def insert_data_query_builder():
+        # создание синхронной сессии с rollback для выполнения запросов к б.д. (в стиле query builder)
+        with sync_engine.connect() as conn:
+            # выполнение запроса к б.д. в стиле query builder
+            conn.execute(
+                insert(workers_table).values(
+                    [
+                        {"username": "Bobr"},
+                        {"username": "Volk"}
+                    ]
+                )
+            )
+
+            # выполнить сохранение в бд
+            conn.commit()
+
+    @staticmethod
+    def select_workers():
+        """Получить записи с бд"""
+        with sync_engine.connect() as conn:
+            query = select(workers_table) # SELECT * FROM workers - подготовка запроса
+            result = conn.execute(query) # выполнение запроса
+            workers = result.one() # получение только одной записи (если больше, то ошибка)
+            # workers = result.all() # получение всех записей
+            # workers = result.one_or_none() # получение 0-1 запись (если больше, то ошибка)
+            # workers = result.scalars().all() # получение всех записей только первого столбца
+            print(workers)
+
+    @staticmethod
+    def update_workers(worker_id: int = 2, name: str = "Misha"):
+        """Обновление значения бд"""
+        with (sync_engine.connect() as conn):
+            query = (
+                update(workers_table)
+                .values(username=name)
+                # .where(workers_table.c.id==worker_id) # 1 способ указать какой элемент обновляем
+                # .filter(workers_table.c.id==worker_id) # 2 способ указать какой элемент обновляем
+                .filter_by(id=worker_id) # 3 способ указать какой элемент обновляем
+            )
+            conn.execute(query)
+            conn.commit()
--- queries/orm.py
+++ queries/orm.py
@@ -1,4 +1,4 @@
-from sqlalchemy import text, insert
+from sqlalchemy import text, insert, select
 from database import sync_engine, async_engine, sync_session_factory, async_session_factory
 from models import metadata_obj, WorkersOrm
 
@@ -9,12 +9,47 @@ def create_tables():
     metadata_obj.create_all() # создать все таблицы
     sync_engine.echo = True # включить логи
 
-# создание синхронной сессии с rollback для выполнения запросов к б.д. (в стиле ORM)
-def insert_data():
-    # создаем обьект записи таблицы
-    worker_bobr = WorkersOrm(username="Bobr")
+class ORMQuery:
+    # создание синхронной сессии с rollback для выполнения запросов к б.д. (в стиле ORM)
+    @staticmethod
+    def insert_data():
+        with sync_session_factory() as sync_session:
+            # создаем обьект записи таблицы
+            worker_bobr = WorkersOrm(username="Bobr")
+            # sync_session.add_all([worker_bobr, worker_bobr]) # добавить несколько записей в сессию
+            sync_session.add(worker_bobr) # добавляем запись в сессию
+            sync_session.flush() # отправляет запросы в бд, но не выполняет их (возвращает данные автоинкремента)
+            sync_session.commit() # сохранить записи
 
-    with sync_session_factory() as sync_session:
-        # sync_session.add_all([worker_bobr, worker_bobr]) # добавить несколько записей в сессию
-        sync_session.add(worker_bobr) # добавляем запись в сессию
-        sync_session.commit() # сохранить записи
+    @staticmethod
+    def select_data():
+        """Получение всех записей из бд"""
+        with sync_session_factory() as sync_session:
+            worker_id = 1
+            query = select(WorkersOrm)  # SELECT * FROM workers - подготовка запроса
+            result = sync_session.execute(query)  # выполнение запроса
+            # workers = result.one()  # получение только одной записи (если больше, то ошибка)
+            workers = result.all() # получение всех записей
+            # workers = result.one_or_none() # получение 0-1 запись (если больше, то ошибка)
+            # workers = result.scalars().all() # получение всех записей только первого столбца
+            print(workers)
+
+    @staticmethod
+    def select_data_one():
+        """Получение одной записи из бд"""
+        with sync_session_factory() as sync_session:
+            worker_id = 1
+            worker_jack = sync_session.get(WorkersOrm, worker_id) # первый способ получения элемента
+            # worker_jack = sync_session.get(WorkersOrm, {"id": worker_id}) # второй способ получения элемента
+            # worker_jack = sync_session.get(WorkersOrm, (worker_id, 'hello')) # третий способ получения элемента
+
+    @staticmethod
+    def update_data(worker_id: int = 2, name: str = "Misha"):
+        """Изменение записи в бд"""
+        with sync_session_factory() as sync_session:
+            worker_jack = sync_session.get(WorkersOrm, worker_id)  # получения элемента
+            worker_jack.username = name # изменение записи
+            # sync_session.expire() # сброс 1 записи в сессии, запрос в базу не отправляет
+            sync_session.expire_all() # сброс всех изменений в сессии, запрос в базу не отправляет
+            # sync_session.refresh(worker_jack) # сброс конкретной записи до значений как в бд
+            sync_session.commit() # сохранение записи
